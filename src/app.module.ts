1be677ac4e255949f4df11b8916b671034234e6f
jhon <jhon.bocanegra26@gmail.com>
2023-04-11 09:28:36 -0500

    modulo de configuracion

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ProductsModule } from './modules/products/products.module';
 import { UsersModule } from './modules/users/users.module';
 import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    ProductsModule,
+    OrdersModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
