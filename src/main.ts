a7795936a01c5193b2d8e2678d5290fb97e1b43f
jhon <jhon.bocanegra26@gmail.com>
2023-04-25 22:50:46 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   const config = new DocumentBuilder()
@@ -27,7 +28,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
